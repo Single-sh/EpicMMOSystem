5677677fe346c2390dba3fd473f31f186a8e70b0
WackyMole <wacky@wackymole.com>
2022-12-06 15:39:52 -0600

    testing UI

---
+++ Drag.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace EpicMMOSystem.MonoScripts;
+
+public class DragWindowCntrl : MonoBehaviour, IBeginDragHandler, IDragHandler// IEndDragHandler, IScrollHandler
+{
+    private float size;
+
+    /// <summary>
+    /// Current scale which is used to keep track whether it is within boundaries
+    /// </summary>
+    private float _scale = 1f;
+
+    /// <summary>
+    /// The minimum scale of the RectTransform Target
+    /// </summary>
+    [SerializeField] private float minimumScale = 0.5f;
+
+    /// <summary>
+    /// The maximum scale of the RectTransform Target
+    /// </summary>
+    [SerializeField] private float maximumScale = 3f;
+
+    /// <summary>
+    /// The scale value it should increase / decrease based on mouse wheel event
+    /// </summary>
+    [SerializeField] private float scaleStep = 0.10f;
+
+    public static void ApplyDragWindowCntrl(GameObject go)
+    {
+        go.AddComponent<DragWindowCntrl>();
+    }
+
+    private RectTransform _window;
+
+    //delta drag
+    private Vector2 _delta;
+
+    private void Awake()
+    {
+        _window = (RectTransform)transform;
+    }
+
+    private void Start()
+    {
+        size = _window.sizeDelta.x;
+        _scale = _window.localScale.x;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _delta = Input.mousePosition - _window.position;
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 newPos = (Vector2)Input.mousePosition - _delta;
+        Vector3 lossyScale = transform.root.lossyScale;
+        Rect rect = _window.rect;
+        Vector2 currentTransform = new(rect.width * lossyScale.x,
+            rect.height * lossyScale.y);
+        Vector2 currOffsetMin, currOffsetMax;
+        Vector2 pivot = _window.pivot;
+        currOffsetMin.x = newPos.x - pivot.x * currentTransform.x;
+        currOffsetMin.y = newPos.y - pivot.y * currentTransform.y;
+        currOffsetMax.x = newPos.x + (1 - pivot.x) * currentTransform.x;
+        currOffsetMax.y = newPos.y + (1 - pivot.y) * currentTransform.y;
+        if (currOffsetMin.x < 0)
+            newPos.x = _window.pivot.x * currentTransform.x;
+        else if (currOffsetMax.x > Screen.width) newPos.x = Screen.width - (1 - _window.pivot.x) * currentTransform.x;
+        if (currOffsetMin.y < 0)
+            newPos.y = _window.pivot.y * currentTransform.y;
+        else if (currOffsetMax.y > Screen.height) newPos.y = Screen.height - (1 - _window.pivot.y) * currentTransform.y;
+        _window.position = newPos;
+    }
+
+    /*
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        MinimalUIPlugin.SaveWindowPositions(_window.gameObject, false);
+    }
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        if (MinimalUIPlugin.KeyboardShortcut.Value.IsPressed())
+        {
+            // Set the new scale
+            float scrollDeltaY = (eventData.scrollDelta.y * scaleStep);
+            float newScaleValue = _scale + scrollDeltaY;
+            ApplyScale(newScaleValue, eventData.position);
+            Player.m_localPlayer.Message(MessageHud.MessageType.Center,
+                $"{_window.gameObject.name} Scroll apply Scale: " + _scale);
+        }
+    }
+    */
+
+    private void ApplyScale(float newScaleValue, Vector2 position)
+    {
+        float newScale = Mathf.Clamp(newScaleValue, minimumScale, maximumScale);
+        // If the scale did not change, don't do anything
+        if (newScale.Equals(_scale)) return;
+
+        // Calculate the local point in the rectangle using the event position
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_window, position, GameCamera.instance.m_camera,
+            out Vector2 localPointInRect);
+        // Set the pivot based on the local point in the rectangle
+        //SetPivot(_window, localPointInRect);
+
+        // Set the new scale
+        _scale = newScale;
+        // Apply the new scale
+       // [8:22 PM]
+        _window.localScale = new Vector3(_scale, _scale, _scale);
+    }
+
+    /// <summary>
+    /// Since this is something I need to remember...
+    /// Sets the pivot based on the local point of the rectangle <see cref="RectTransformUtility.ScreenPointToLocalPointInRectangle"/>.
+    /// Keeps the RectTransform in place when changing the pivot by countering the position change when the pivot is set.
+    /// </summary>
+    /// <param name="rectTransform">The target RectTransform</param>
+    /// <param name="localPoint">The local point of the target RectTransform</param>
+    private void SetPivot(RectTransform rectTransform, Vector2 localPoint)
+    {
+        // Calculate the pivot by normalizing the values
+        Rect targetRect = rectTransform.rect;
+        float pivotX = (float)((localPoint.x - (double)targetRect.x) / (targetRect.xMax - (double)targetRect.x));
+        float pivotY = (float)((localPoint.y - (double)targetRect.y) / (targetRect.yMax - (double)targetRect.y));
+        Vector2 newPivot = new Vector2(pivotX, pivotY);
+
+        // Delta pivot = (current - new) * scale
+        Vector2 deltaPivot = (rectTransform.pivot - newPivot) * _scale;
+        // The delta position to add after pivot change is the inversion of the delta pivot change * size of the rect * current scale of the rect
+        Vector2 rectSize = targetRect.size;
+        Vector3 deltaPosition = new Vector3(deltaPivot.x * rectSize.x, deltaPivot.y * rectSize.y) * -1f;
+
+        // Set the pivot
+        rectTransform.pivot = newPivot;
+        rectTransform.localPosition += deltaPosition;
+    }
+}
---
+++ PanelRefs.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace EpicMMOSystem;
+
+public class ExpPanelRefs : MonoBehaviour
+{
+    [SerializeField] private RectTransform ExpPanelRoot;
+    [SerializeField] private RectTransform ExpPanelBkg;
+    [SerializeField] private Image ExpPanelBarFil;
+    [SerializeField] private Text ExpPanelLvlTxt;
+    [SerializeField] private Text ExpPanelExpTxt;
+}
+
+public class EpicHudPanelRefs : MonoBehaviour
+{
+    [SerializeField] private RectTransform ExpPanelRoot;
+    [SerializeField] private RectTransform ExpPanelBkg;
+    [SerializeField] private Image HealthBarFil;
+    [SerializeField] private Image StaminaBarFil;
+    [SerializeField] private Image EitrBarFil;
+    [SerializeField] private Text EpicHudPanelLvlTxt;
+    [SerializeField] private Text EpicHudPanelExpTxt;
+}
+
+public class FriendCellPanelRefs : MonoBehaviour
+{
+    [SerializeField] private RectTransform PanelRoot;
+    [SerializeField] private RectTransform PanelBkg;
+    [SerializeField] private Image IconClass;
+    [SerializeField] private Text PlayerName;
+    [SerializeField] private Text Level;
+    [SerializeField] private Text Status;
+    [SerializeField] private RectTransform ButtonGroup;
+    [SerializeField] private Button AddGroup;
+    [SerializeField] private Button AcceptButton;
+    [SerializeField] private Button DeclineButton;
+}
+
+public class LevelHudPanelRefs : MonoBehaviour
+{
+    // MenuPanel Stuff
+    [Header("MenuPanel")]
+    [SerializeField] private RectTransform CanvasRoot;
+    [SerializeField] private RectTransform MenuPanelRoot;
+    [SerializeField] private RectTransform MenuPanelBkg;
+    [SerializeField] private RectTransform ButtonGroup;
+    [SerializeField] private RectTransform ButtonLevelSystem;
+    [SerializeField] private RectTransform ButtonFriends;
+    [SerializeField] private RectTransform ButtonQuests;
+    [SerializeField] private RectTransform ButtonProfessions;
+    [SerializeField] private RectTransform ButtonShop;
+
+    // PointPanel Stuff
+    [Header("PointPanel")]
+    [SerializeField] private RectTransform PointPanelRoot;
+    [SerializeField] private RectTransform PointPanelBkg;
+    [SerializeField] private Text HeaderText;
+    [SerializeField] private RectTransform PointSectionRoot;
+    [SerializeField] private RectTransform StrengthRoot;
+    [SerializeField] private Text StrengthText;
+    [SerializeField] private Button StrengthP1Button;
+    [SerializeField] private Button StrengthP5Button;
+    [SerializeField] private Text AgilityText;
+    [SerializeField] private Button AgilityP1Button;
+    [SerializeField] private Button AgilityP5Button;
+    [SerializeField] private Text IntellectText;
+    [SerializeField] private Button IntellectP1Button;
+    [SerializeField] private Button IntellectP5Button;
+    [SerializeField] private Text BodyText;
+    [SerializeField] private Button BodyP1Button;
+    [SerializeField] private Button BodyP5Button;
+    [SerializeField] private GameObject DummyListStat;
+    [SerializeField] private Text FreePointText;
+    [SerializeField] private Button FreePointApply;
+    [SerializeField] private Button FreePointCancel;
+    [SerializeField] private Button PointResetButton;
+    [SerializeField] private Text CurrentLvlText;
+    [SerializeField] private Text CurrentExpText;
+    [SerializeField] private RectTransform DescStatsContentParent;
+    [SerializeField] private RectTransform DescStatsDummyHeader;
+
+    [SerializeField] private Text DescStatsDummyText;
+
+    // ApplyResetPanel Stuff
+    [Header("ApplyResetPanel")]
+    [SerializeField] private RectTransform ApplyResetPanelRoot;
+    [SerializeField] private RectTransform ApplyResetPanelBkg;
+    [SerializeField] private RectTransform ApplyResetButtonGroup;
+    [SerializeField] private Button ButtonYes;
+    [SerializeField] private Button ButtonNo;
+    [SerializeField] private Text Text;
+
+    // FriendsListPanel Stuff
+    [Header("FriendsListPanel")]
+    [SerializeField] private RectTransform FriendsListPanelRoot;
+    [SerializeField] private RectTransform FriendsListPanelBkg;
+    [SerializeField] private RectTransform FriendsListPanelHeader;
+    [SerializeField] private Image BorderImage;
+    [SerializeField] private GameObject HeaderInvited;
+    [SerializeField] private GameObject Invited;
+    [SerializeField] private GameObject HeaderFriends;
+    [SerializeField] private GameObject Friends;
+    [SerializeField] private Text FriendsListPanelText;
+    [SerializeField] private Button AddFriendButton;
+
+    // SendInvitePanel Stuff
+    [Header("SendInvitePanel")]
+    [SerializeField] private RectTransform SendInvitePanelRoot;
+    [SerializeField] private RectTransform SendInvitePanelBkg;
+    [SerializeField] private InputField SendInviteInputField;
+    [SerializeField] private Button SendButton;
+    [SerializeField] private Text SendText;
+    [SerializeField] private Button CancelButton;
+    [SerializeField] private Text CancelText;
+}
